9ddf1f81dc65945297046b1484cb1b24da343bc6
Malik Graham <malikgraham15@yahoo.com>
2017-12-28 02:07:33 -0500

    Initial Commit

---
+++ MoodProveMacApp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MoodProveMacApp
+//
+//  Created by Malik Graham on 12/28/17.
+//  Copyright © 2017 Malik Graham. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
