1c404fdb93e0cada8ca217fcc6a5e18bb1e3881e
Malik Graham <malikgraham15@yahoo.com>
2018-02-13 15:31:10 -0600

    Field registration items are successfully retrieved and check

--- MoodProveMacApp/RegisterViewController.swift
+++ MoodProveMacApp/RegisterViewController.swift
@@ -9,6 +9,8 @@
 import Cocoa
 
 class RegisterViewController: NSViewController {
+    
+    let options = ["4:00 AM", "5:00 AM", "6:00 AM", "7:00 AM", "8:00 AM", "9:00 AM", "10:00 AM"]
 
     @IBOutlet weak var name: NSTextField!
     
@@ -20,8 +22,27 @@ class RegisterViewController: NSViewController {
     
     @IBOutlet weak var wakeUpTime: NSPopUpButton!
     
+    @IBOutlet weak var warning: NSTextField!
+    
+    @IBAction func wakeUpButtonSelected(_ sender: Any) {
+        print(wakeUpTime.titleOfSelectedItem!)
+    }
+    
+    
     @IBAction func register(_ sender: Any) {
-        
+        if (name.stringValue.isEmpty || email.stringValue.isEmpty
+            || password.stringValue.isEmpty || passwordConfirmation.stringValue.isEmpty) {
+            warning.isHidden = false
+            warning.stringValue = "You left required input fields empty!"
+            return
+        }
+        else if (password != passwordConfirmation) {
+            warning.isHidden = false
+            warning.stringValue = "Your passwords do not match!"
+            return
+        }
+        warning.isHidden = true
+        handleRegistration(name: name.stringValue, email: email.stringValue, password: password.stringValue, wakeUpTime: wakeUpTime.titleOfSelectedItem!)
     }
     
     @IBAction func loginClicked(_ sender: Any) {
@@ -32,6 +53,17 @@ class RegisterViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        warning.isHidden = true
+        buildWakeUpTimeButton()
+    }
+    
+    func buildWakeUpTimeButton() {
+        wakeUpTime.removeAllItems()
+        wakeUpTime.addItems(withTitles: options)
+    }
+    
+    func handleRegistration(name: String, email: String, password: String, wakeUpTime: String) {
+        print("Handling registration")
     }
     
 }
