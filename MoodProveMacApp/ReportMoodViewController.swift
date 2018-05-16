da2b6ca4ea8dbed3f2a4586b26b0df619b09bd5c
Malik Graham <malikgraham15@yahoo.com>
2018-05-15 22:18:16 -0500

    Report mood components connected and now operational

--- MoodProveMacApp/ReportMoodViewController.swift
+++ MoodProveMacApp/ReportMoodViewController.swift
@@ -11,10 +11,32 @@ import Cocoa
 class ReportMoodViewController: NSViewController {
     
     var userId: String?
+    
+    let moodModifiersOptions = ["--", "anxious", "calm", "confused", "overwhelmingly"]
+    
+    let baseMoodsOptions = ["--", "happy", "mad", "neutral", "sad", "stressed"]
 
+    @IBOutlet weak var moodModifiers: NSPopUpButton!
+    
+    @IBOutlet weak var baseMoods: NSPopUpButton!
+    
+    @IBOutlet weak var currentMoodLabel: NSTextField!
+    
+    @IBAction func reportMood(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        buildPopUpButtons()
+    }
+    
+    func buildPopUpButtons() {
+        moodModifiers.removeAllItems()
+        baseMoods.removeAllItems()
+        moodModifiers.addItems(withTitles: moodModifiersOptions)
+        baseMoods.addItems(withTitles: baseMoodsOptions)
     }
     
 }
